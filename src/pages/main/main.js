142860d926b07d92c89d15cbdb85f25cdf674c1f
joy9 developer <admin@joy9.cc>
2023-06-24 15:15:09 +0800

    add new code

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import { Carousel, Row, Col, Button  } from 'antd';
 import './main.less';
 import logo192 from "./img/logo192.png";
+//import logoSmall from "./img/logo_small.png";
+
+export function isMobileDevice(navigator) {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
 export default class Main extends Component {
   constructor(props) {
     super(props);
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
+    this.userOnMobileDevice = "navigator" in window && isMobileDevice(window.navigator);
   }
   next() {
     this.slider.next();
@@ -16,23 +22,46 @@ export default class Main extends Component {
     this.slider.prev();
   }
   logo() {
+    if (this.userOnMobileDevice) {
+      return (
+        <a href="/" className="logo"><img src={logo192} style={{ width: "40px", marginBottom: "5px", borderRadius: "30%", boxShadow: "0 0 1em #000"}} alt="Logo"></img>Joy9 Exchange</a>
+      )
+    }
+
     return (
       <a href="/" className="logo"><img src={logo192} style={{ width: "80px", marginBottom: "20px", borderRadius: "50%", boxShadow: "0 0 1em #000"}} alt="Logo"></img> Joy9 Exchange</a>
     )
   }
   nav() {
+    if (this.userOnMobileDevice) {
+      return (
+        <nav className="nav">
+          <Row justify="space-between" align="bottom">
+            <Col>
+              <a className="navLink" href="https://doc.joy9.io/" rel="noreferrer noopenner" target="_blank">Docs</a>
+            </Col>
+            <Col>
+              <Button type="primary" size="large" style={{marginLeft: "10px", borderRadius: "10px", boxShadow: "0 0 1em #333"}}>
+                <a style={{fontWeight: "bold"}} href="https://app.joy9.io/" rel="noreferrer noopenner" target="_blank">Launch App</a>
+              </Button>
+            </Col>
+          </Row>
+        </nav>
+      )
+    }
+
     return (
       <nav className="nav">
         <Row justify="space-between" align="bottom">
           <Col>
-            <a className="navLink" href="https://doc.joy9.cc/" rel="noreferrer noopenner" target="_blank">Docs</a>
+            <a className="navLink" href="https://doc.joy9.io/" rel="noreferrer noopenner" target="_blank">Docs</a>
           </Col>
           <Col>
-            <a className="navLink" href="https://doc.joy9.cc/faq" rel="noreferrer noopenner" target="_blank">FAQ</a>
+            <a className="navLink" href="https://doc.joy9.io/faq" rel="noreferrer noopenner" target="_blank">FAQ</a>
           </Col>
           <Col>
             <Button type="primary" size="large" style={{marginLeft: "20px", borderRadius: "10px", boxShadow: "0 0 1em #333"}}>
-              <a style={{fontWeight: "bold"}} href="https://app.joy9.cc/" rel="noreferrer noopenner" target="_blank">Launch App</a>
+              <a style={{fontWeight: "bold"}} href="https://app.joy9.io/" rel="noreferrer noopenner" target="_blank">Launch App</a>
             </Button>
           </Col>
         </Row>
@@ -42,8 +71,10 @@ export default class Main extends Component {
   footer() {
     return (
       <div className="footerContent">
-        <p style={{ marginBottom: "20px", fontSize: "54px", color: "#eee", fontWeight: "bold" }}>Joy9 Exchange <span style={{ fontWeight: "normal" }}>Protocol</span></p>
-        <p style={{ fontSize: "24px" }}>exchange spot and perpetual, and earn on the <a href="https://arbitrum.io/" rel="noreferrer noopenner" target="_blank">#Arbitrum</a></p>
+        {!this.userOnMobileDevice && (
+          <p style={{ marginBottom: "20px", fontSize: "54px", color: "#eee", fontWeight: "bold" }}>Joy9 Exchange <span style={{ fontWeight: "normal" }}>Protocol</span></p>
+          )}
+        <p style={{ fontSize: "24px" }}>exchange spot and perpetual, and earn on the <a href="https://base.org/" rel="noreferrer noopenner" target="_blank">#Base</a></p>
         <div>
           <a href="https://twitter.com/joy9exchange" rel="noreferrer noopenner" target="_blank" className="footerLink" ><svg width="32px" height="32px" fill="white" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M31.275 5.924c-.503.223-1.02.411-1.548.564a6.842 6.842 0 0 0 1.393-2.45.516.516 0 0 0-.755-.6 12.181 12.181 0 0 1-3.6 1.422 6.904 6.904 0 0 0-4.809-1.96 6.877 6.877 0 0 0-6.813 7.757A17.587 17.587 0 0 1 3.072 4.253a.516.516 0 0 0-.846.067 6.866 6.866 0 0 0-.93 3.454c0 1.655.59 3.225 1.634 4.452a5.816 5.816 0 0 1-.919-.41.516.516 0 0 0-.767.44v.091c0 2.47 1.33 4.695 3.362 5.908a5.882 5.882 0 0 1-.522-.076.516.516 0 0 0-.588.664 6.867 6.867 0 0 0 5.032 4.605 12.153 12.153 0 0 1-6.497 1.854c-.486 0-.976-.029-1.455-.085a.517.517 0 0 0-.339.946A18.525 18.525 0 0 0 10.255 29.1c6.994 0 11.37-3.298 13.808-6.065 3.041-3.45 4.785-8.016 4.785-12.528 0-.189-.003-.38-.009-.569a13.355 13.355 0 0 0 3.074-3.255.516.516 0 0 0-.638-.759Z"></path></svg></a>
           <a href="https://github.com/joy9-exchange" rel="noreferrer noopenner" target="_blank" className="footerLink" ><svg width="32px" height="32px" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg></a>
